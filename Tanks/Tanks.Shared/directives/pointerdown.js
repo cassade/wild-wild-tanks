7de2e9140882de0f955a138307265f1bff53509a
plotyan <plotyan@inbox.ru>
2014-11-28 21:46:19 +0300

    Change clicks to pointer events

---
+++ Tanks/Tanks.Shared/directives/pointerdown.js
@@ -0,0 +1,14 @@
+﻿angular.module('tanks')
+    .directive('pointerdown', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function ($scope, element, attrs) {
+                var handler = $parse(attrs.pointerdown);
+                element[0].addEventListener('pointerdown', function (e) {
+                    handler($scope, {
+                        $event: e
+                    });
+                });
+            },
+        };
+    });
---
+++ Tanks/Tanks.Shared/directives/pointerup.js
@@ -0,0 +1,16 @@
+﻿angular.module('tanks')
+    .directive('pointerup', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function ($scope, element, attrs) {
+                var targetHandler = $parse(attrs.pointerup);
+                var sourceHandler = function (e) {
+                    targetHandler($scope, {
+                        $event: e
+                    });
+                };
+                element[0].addEventListener('pointerup' , sourceHandler);
+                element[0].addEventListener('pointerout', sourceHandler);
+            },
+        };
+    });
